164edac33f791120cf13f2ee1587f2ece08e00d2
Leandrns <leandro2005souza@gmail.com>
2025-08-27 18:56:08 -0300

    Init project structure

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+	plugins: [react()],
+	server: {
+		https: true, // Necessário para WebXR
+		host: true,
+	},
+	optimizeDeps: {
+		include: ["three", "jsqr"],
+	},
+});
