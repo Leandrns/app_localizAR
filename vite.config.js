8ed354c4261684000b1a03de91756f8533c20b28
Leandrns <leandro2005souza@gmail.com>
2025-08-27 19:26:01 -0300

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		https: true, // Necessário para WebXR
-		host: true,
-	},
-	optimizeDeps: {
-		include: ["three", "jsqr"],
-	},
+	// server: {
+	// 	https: true, // Necessário para WebXR
+	// 	host: true,
+	// },
+	// optimizeDeps: {
+	// 	include: ["three", "jsqr"],
+	// },
 });
