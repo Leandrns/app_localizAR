508aec74db3275c2ba0e55652f939677b6cc12f9
Leandrns <leandro2005souza@gmail.com>
2025-09-25 10:49:26 -0300

    test: mudando tamanho na imagem

--- src/components/ARView.jsx
+++ src/components/ARView.jsx
@@ -736,7 +736,7 @@ function ARView({ mode, calibrado, pontoReferencia, pontos, onCreatePoint }) {
 							zIndex: 1,
 							position: "relative"
 						}}>
-							<img src={currentPrize.urlImg} alt="" />
+							<img src={currentPrize.urlImg} alt="" style={{width: "100px"}}/>
 						</div>
 						
 						<h3 style={{ 
